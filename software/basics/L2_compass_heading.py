9b54fc441b72d328810f4ce7e420c4b3773c86fd
Brey Caraway <breycaraway@gmail.com>
2023-01-15 15:15:38 -0600

    compass correction

--- software/basics/L2_compass_heading.py
+++ software/basics/L2_compass_heading.py
@@ -1,6 +1,6 @@
-# This progrom is used to enter the calibrated magnitometer matrix into "mag_calibration" in line 16. 
-# Do so after calibrating the magnitemeter using the "Magnetometer_Compass_Calibration.ipynb" jupyter notebook.
-# The calibrated magnietometer values can be found in the output from the fourth cell beginning with "Final calibration in uTesla: [ ]"
+# This program is used to enter the calibrated magnetometer matrix into "mag_calibration" in line 24. 
+# Do so after calibrating the magnetometer using the "Magnetometer_Compass_Calibration.ipynb" jupyter notebook.
+# The calibrated magnetometer values can be found in the output from the fourth cell beginning with "Final calibration in uTesla: [ ]"
 # The code will then determine the heading angle of the robot in degrees and print it to the terminal.
 
 # Import libraries
@@ -13,19 +13,29 @@ i2c = board.I2C()
 imu = adafruit_bno055.BNO055_I2C(i2c)
 imu.mode = adafruit_bno055.MAGONLY_MODE
 
-declination_angle = 7           # define the declination angle for the robot's current geographical location
+# The magnetic declination used to offset the heading for a more accurate true north. 
+# A declination of 7 is used for Mid-East Texas.
+declination_angle = 7
 
-mag_calibration = [52.4375, 4.0, 123.71875]         #note that you need to put in your calibrated magnitamotor matrix
+# Insert the final calibrated magnetometer matrix in line 14 below. This is gathered from the print statement
+# in the "Calibrate the Magnetometer" Section of the "Magnetometer_Compass_Calibration.ipynb" jupyter notebook.
+# You can copy the matrix out right from the print statement and paste it into the "mag_calibration" list below.
+# Place holders of 0.00 have been used for the x,y,z values.
+mag_calibration = [0.00, 0.00, 0.00]
 
-while True:
-        
+def get_heading():
+    
     x, y, z = imu.magnetic
 
     x -= mag_calibration[0]
     y -= mag_calibration[1]
     z -= mag_calibration[2]
-    
-    heading = np.arctan2(y,x)
-    print(np.degrees(heading)-declination_angle)
-    
-    time.sleep(0.2)
+
+    heading = np.degrees(np.arctan2(y,x))-declination_angle         # Calculate the compass heading in degrees
+
+    return heading
+
+if __name__ == "__main__":
+    while True:
+        print(round(get_heading(),2))           # Print the compass heading in degrees
+        time.sleep(0.1)
